719e0fba6ff0f4f5c05a0af6ade4cfc184ffce34
Andrei Lorin <andreilorin@gmail.com>
2020-07-24 14:30:17 +0100

    Initial commit

---
+++ src/main/java/com/aeontree/springci/SpringciApplication.java
@@ -0,0 +1,13 @@
+package com.aeontree.springci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringciApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aeontree/springci/SpringciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeontree.springci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
