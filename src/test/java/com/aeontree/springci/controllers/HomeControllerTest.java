47e99b3adff18670e4c3f60b9660db1ee5ace2bd
Andrei Lorin <andreilorin@gmail.com>
2020-09-14 18:30:58 +0100

    add test dependencies

---
+++ src/test/java/com/aeontree/springci/controllers/HomeControllerTest.java
@@ -0,0 +1,15 @@
+package com.aeontree.springci.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class HomeControllerTest {
+
+    @Test
+    void testAddition() {
+        assertThat(1).isEqualTo(1);
+    }
+
+}
